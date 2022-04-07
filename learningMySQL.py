3940971c8bb4fd806085b6b279279af56bce1701
Farhan Hasan <hasanfarhan33@gmail.com>
2022-04-07 15:24:02 +0530

    Day 33

--- learningMySQL.py
+++ learningMySQL.py
@@ -36,8 +36,62 @@ mycursor = db.cursor()
 
 # ALTERING TABLES
 # mycursor.execute("ALTER TABLE Test ADD COLUMN food VARCHAR(50) NOT NULL")
-mycursor.execute("ALTER TABLE Test DROP food")
-mycursor.execute("DESCRIBE Test")
-for x in mycursor:
-    print(x)
 
+
+users = [("Timothy", "screwTimothyFromCanada", "2345", "iamjealousoftimothy@gmail.com"),
+         ("Paul", "paulFromTheBeatles", "johnisdead1", "liveandletdie@gmail.com"),
+         ("George", "georgeFromTheBeatles", "johnisdefinitelydead1", "nooneremembersmysongs@gmail.com"),
+         ("Ringo", "ringoFromTheBeatles", "imissmyumbrella1", "whereismyumbrella@gmail.com"),
+         ("John", "johnFromTheBeatles", "bulletinagun1", "thereisdefinitelynoheaven@gmail.com"),
+         ("Jerry", "jerryFromSeinfeld", "passwordsareuseless", "kramerisannoying@gmail.com"),
+         ("George", "georgeFromSeinfeld", "worldsarecolliding", "halfprice@gmail.com"),
+         ("Elaine", "elaineFromSeinfeld", "johnfkennedyjunior", "laidoutforme@gmail.com"),
+         ("Kramer", "kramerFromSeinfeld", "kosmokramer", "pigmanisoutthere@gmail.com"),
+         ("Desiree", "emma_pfann1977", "Xah6chohSae", "paolo.zbonc@hotmail.com"),
+         ("Nancy", "bryan", "aiThoh5och", "nasir2004@gmail.com"),
+         ("Armando", "ceasar.med1974", "noh8Ujae", "garrick2007@hotmail.com"),
+         ("Gary", "garywithausername1", "IeGah3iep5", "viviane2011@gmail.com"),
+         ("Sandra", "Bivaliner", "shu7aYah", "gudrun_macejkov@gmail.com"),
+         ("Christy", "andrew", "ahCh6eed9", "corine2009@hotmail.com"),
+         ("Gary", "ultraspikes", "sojaro5Th", "ila_ryan1972@gmail.com"),
+         ("John", "ashlee", "bee5Umud", "carolyn.torp@gmail.com"),
+         ("Neal", "adelbert.p2008", "hooHee6eeVai", "darius1983@yahoo.com"),
+         ("Johnny", "TheHollowCarter", "piquoh8Uong", "kasandra1981@hotmail.com"),
+         ("Martha", "elia", "laht6IGohj1", "carli.oreil@gmail.com")]
+
+userScores = [(54, 96),
+              (63, 34),
+              (11, 87),
+              (83, 82),
+              (90, 85),
+              (8, 39),
+              (23, 95),
+              (23 ,43),
+              (33, 94),
+              (32 ,38),
+              (74, 83),
+              (21, 19),
+              (54, 87),
+              (45, 54),
+              (37, 56),
+              (89, 35),
+              (98, 28),
+              (54, 49),
+              (28, 95),
+              (39, 69)]
+
+# Q1 = "CREATE TABLE Users (id int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), username VARCHAR(50), password VARCHAR(50), emailAddress VARCHAR(50))"
+# Q2 = "CREATE TABLE Scores (userId int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES Users(id), game1 int DEFAULT 0, game2 int DEFAULT 0)"
+Q3 = "INSERT INTO Users (name, username, password, emailAddress) VALUES (%s, %s, %s, %s)"
+Q4 = "INSERT INTO Scores (userId, game1, game2) VALUES (%s, %s, %s)"
+
+# for x, user in enumerate(users):
+#     mycursor.execute(Q3, user)
+#     lastId = mycursor.lastrowid
+#     mycursor.execute(Q4, (lastId,) + userScores[x])
+
+# db.commit()
+# mycursor.execute(Q1)
+# mycursor.execute(Q2)
+
+# mycursor.executemany("INSERT INTO Users (name, username, password, emailAddress) VALUES (%s, %s, %s, %s)", users)
